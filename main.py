87873d5eb1201cb43df4de99c0e49ed2926e743a
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-12-18 10:52:45 -0300

    adiciona arquivo de resultados

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ normalizer = BasicTextNormalizer()
 dictionary_keys = []
 
 def main():
-    open('output.txt', 'w').close()
+    open('results.txt', 'w').close()
 
     official_transcriptions = get_all_official_transcriptions('transcriptions')
     whisper_transcriptions = transcribe_all_audios_from_directory('audio')
@@ -80,7 +80,7 @@ def get_transcription_value(official_transcriptions, whisper_transcriptions, key
         sys.exit('A transcrição do arquivo ' + key + ' não foi encontrada!')
 
 def save_to_file(officcial_transcription, whisper_transcription):
-    f = open('output.txt', 'a')
+    f = open('results.txt', 'a')
 
     if (os.stat(f.name).st_size == 0):
         f.write('Tamanho do modelo: ' + model_size.capitalize() + '\n')
